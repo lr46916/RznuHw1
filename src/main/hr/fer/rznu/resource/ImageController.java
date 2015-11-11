eaeca94b75565b10a5ef79dd11fc2a5ee80187b4
tim <leonrotim@gmail.com>
2015-11-11 02:05:15 +0100

    posts

--- src/main/hr/fer/rznu/resource/ImageController.java
+++
@@ -1,5 +0,0 @@
-package hr.fer.rznu.resource;
-
-public class ImageController {
-
-}
---
+++ src/main/hr/fer/rznu/resource/PostsController.java
@@ -0,0 +1,10 @@
+package hr.fer.rznu.resource;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PostsController {
+	
+	
+	
+}
